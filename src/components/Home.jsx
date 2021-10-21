f889f4f40ad77747d1ab7cebe816cd895560f862
funk93 <vojka1993@gmail.com>
2021-10-21 11:30:57 +0200

    useFetch custom hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import useFetch from "../useFetch";
+import BlogList from "./BlogList";
+
+//npx json-server --watch data/db.json --port 8000
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-  return <div className="home"></div>;
+  const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
+
+  return (
+    <div className="home">
+      {/* logical && or conditional templating */}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
 };
 
 export default Home;
